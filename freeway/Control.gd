f8e7e47204a64c6cff36a5e48e0da4763257f523
Gabriel Hérisson <88601359+GabrielHerisson@users.noreply.github.com>
2024-05-15 12:52:58 -0300

    Menu

--- freeway/Control.gd
+++ freeway/Control.gd
@@ -1,16 +1,19 @@
 extends Control
 
 func _ready() -> void:
+	pass
+#		button.connect("mouse_exited", mouse_interaction (button))
+#		button.connect("mouse_entered", mouse_interaction (button))
+		
+		
+func _process(delta):
 	for button in get_tree().get_nodes_in_group("button"):
-		button.connect("pressed", self, "on_button_pressed", [button])
-		button.connect("mouse_exited", self, "mouse_interaction", [button, "exited"])
-		button.connect("mouse_entered", self, "mouse_interaction", [button, "entered"])
+		button.connect("pressed", Callable(self,"_on_Button_pressed").bind(button))
 		
-		
-func on_button_pressed(buttom: Button) -> void:
+func _on_button_pressed(buttom: Button):
 	match buttom.name:
 		"Play":
-			var _game: bool = get_tree().change_scene("res://main.tscn")
+			var _game: bool = get_tree().change_scene_to_file("res://main.tscn")
 			
 		"Quit":
 			get_tree().quit()
