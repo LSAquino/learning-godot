33f047b87a1c6731b6e7ff25e55d70a5089a292e
Gabriel Hérisson <88601359+GabrielHerisson@users.noreply.github.com>
2024-05-15 14:09:28 -0300

    .

--- freeway/Control.gd
+++ freeway/Control.gd
@@ -2,22 +2,14 @@ extends Control
 
 func _ready() -> void:
 	pass
-#		button.connect("mouse_exited", mouse_interaction (button))
-#		button.connect("mouse_entered", mouse_interaction (button))
-		
-		
+
 func _process(delta):
 	for button in get_tree().get_nodes_in_group("button"):
 		button.connect("pressed", Callable(self,"_on_Button_pressed").bind(button))
+		button.connect("mouse_exited", Callable(self,"mouse_interaction").bind(button))
+		button.connect("mouse_entered", Callable(self,"mouse_interaction").bind(button))
 		
-func _on_button_pressed(buttom: Button):
-	match buttom.name:
-		"Play":
-			var _game: bool = get_tree().change_scene_to_file("res://main.tscn")
-			
-		"Quit":
-			get_tree().quit()
-	
+
 func mouse_interaction(buttom: Button, state: String) -> void:
 	match state:
 		"exited":
@@ -25,3 +17,9 @@ func mouse_interaction(buttom: Button, state: String) -> void:
 			
 		"exited":
 			buttom.modulate.a = 0.5
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://main.tscn")
+
+func _on_quit_pressed():
+	get_tree().quit()
